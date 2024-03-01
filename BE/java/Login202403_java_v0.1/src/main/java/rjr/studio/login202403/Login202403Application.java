882202438d2f668dde01a8a1b8955d83287da3cc
PC-ONE\j <rjr.b2b>
2024-03-01 17:01:30 +0100

    Aggiornato le dipendenze e create i file base

---
+++ BE/java/Login202403_java_v0.1/src/main/java/rjr/studio/login202403/Login202403Application.java
@@ -0,0 +1,14 @@
+package rjr.studio.login202403;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Login202403Application {
+
+	public static void main(String[] args) {
+		System.setProperty("spring.devtools.restart.enabled", "false");
+		SpringApplication.run(Login202403Application.class, args);
+	}
+
+}
---
+++ BE/java/Login202403_java_v0.1/src/test/java/rjr/studio/login202403/Login202403ApplicationTests.java
@@ -0,0 +1,13 @@
+package rjr.studio.login202403;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class Login202403ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+	
+}
